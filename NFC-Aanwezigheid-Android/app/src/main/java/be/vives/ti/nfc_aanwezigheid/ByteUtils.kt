152f5e83d69bf60c01b9ac16f6feda60a29397ac
VanHaverbekeBram <92434333+VanHaverbekeBram@users.noreply.github.com>
2023-02-28 16:58:32 +0100

    id eruit halen
    blijkt wel null te zijn bij eerste test

---
+++ NFC-Aanwezigheid-Android/app/src/main/java/be/vives/ti/nfc_aanwezigheid/ByteUtils.kt
@@ -0,0 +1,18 @@
+package be.vives.ti.nfc_aanwezigheid
+
+class ByteUtils {
+    companion object {
+        fun isNullOrEmpty(array: ByteArray?): Boolean {
+            if (array == null) {
+                return true
+            }
+            val length = array.size
+            for (i in 0 until length) {
+                if (array[i].toInt() != 0) {
+                    return false
+                }
+            }
+            return true
+        }
+    }
+}
--- NFC-Aanwezigheid-Android/app/src/main/java/be/vives/ti/nfc_aanwezigheid/MainActivity.kt
+++ NFC-Aanwezigheid-Android/app/src/main/java/be/vives/ti/nfc_aanwezigheid/MainActivity.kt
@@ -1,60 +1,109 @@
 package be.vives.ti.nfc_aanwezigheid
 
 import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
-import android.content.IntentFilter
+import android.nfc.FormatException
 import android.nfc.NfcAdapter
-import android.nfc.tech.NfcF
-import androidx.appcompat.app.AppCompatActivity
+import android.nfc.NfcManager
+import android.nfc.Tag
 import android.os.Bundle
-import android.provider.Settings
-import android.view.View
+import android.util.Log
 import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private var intentFiltersArray: Array<IntentFilter>? = null
-    private val techListsArray = arrayOf(arrayOf(NfcF::class.java.name))
-    private val nfcAdapter: NfcAdapter? by lazy {
-        NfcAdapter.getDefaultAdapter(this)
-    }
-    private var pendingIntent: PendingIntent? = null
+
+    private var adapter: NfcAdapter? = null
+    var tag: WritableTag? = null
+    var tagId: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        try {
-            if (nfcAdapter == null) {
-                val builder = AlertDialog.Builder(this@MainActivity)
-                builder.setMessage("This device doesn't support NFC.")
-                builder.setPositiveButton("Cancel", null)
-                val myDialog = builder.create()
-                myDialog.setCanceledOnTouchOutside(false)
-                myDialog.show()
-                //txtviewshopid.setText("THIS DEVICE DOESN'T SUPPORT NFC. PLEASE TRY WITH ANOTHER DEVICE!")
-                //txtviewmachineid.visibility = View.INVISIBLE
 
-            } else if (!nfcAdapter!!.isEnabled) {
-                val builder = AlertDialog.Builder(this@MainActivity)
-                builder.setTitle("NFC Disabled")
-                builder.setMessage("Plesae Enable NFC")
-                //txtviewshopid.setText("NFC IS NOT ENABLED. PLEASE ENABLE NFC IN SETTINGS->NFC")
-                //txtviewmachineid.visibility = View.INVISIBLE
+        initNfcAdapter()
+        initViews()
+    }
 
-                builder.setPositiveButton("Settings") { _, _ -> startActivity(Intent(Settings.ACTION_NFC_SETTINGS)) }
-                builder.setNegativeButton("Cancel", null)
-                val myDialog = builder.create()
-                myDialog.setCanceledOnTouchOutside(false)
-                myDialog.show()
-            }
-        }
-        catch (ex: Exception){
-            Toast.makeText(applicationContext, ex.message, Toast.LENGTH_SHORT).show()
+    private fun initNfcAdapter() {
+        val nfcManager = getSystemService(Context.NFC_SERVICE) as NfcManager
+        adapter = nfcManager.defaultAdapter
+    }
+
+    private fun initViews() {
+        write_tag.setOnClickListener {
+            writeNDefMessage()
         }
     }
+
+    private fun writeNDefMessage() {
+        val message = NfcUtils.prepareMessageToWrite(StringUtils.randomString(44), this)
+        val writeResult = tag!!.writeData(tagId!!, message)
+        if (writeResult) {
+            showToast("Write successful!")
+        } else {
+            showToast("Write failed!")
+        }
+    }
+
+    fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
     override fun onResume() {
         super.onResume()
-        nfcAdapter?.enableForegroundDispatch(this, pendingIntent, intentFiltersArray, techListsArray)
+        enableNfcForegroundDispatch()
     }
 
+    override fun onPause() {
+        disableNfcForegroundDispatch()
+        super.onPause()
+    }
+
+    private fun enableNfcForegroundDispatch() {
+        try {
+            val intent = Intent(this, javaClass).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+            val nfcPendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+            adapter?.enableForegroundDispatch(this, nfcPendingIntent, null, null)
+        } catch (ex: IllegalStateException) {
+            Log.e(getTag(), "Error enabling NFC foreground dispatch", ex)
+        }
+    }
+
+    private fun disableNfcForegroundDispatch() {
+        try {
+            adapter?.disableForegroundDispatch(this)
+        } catch (ex: IllegalStateException) {
+            Log.e(getTag(), "Error disabling NFC foreground dispatch", ex)
+        }
+    }
+
+    private fun getTag() = "MainActivity"
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        val tagFromIntent = intent.getParcelableExtra<Tag>(NfcAdapter.EXTRA_TAG)
+        try {
+            tag = tagFromIntent?.let { WritableTag(it) }
+        } catch (e: FormatException) {
+            Log.e(getTag(), "Unsupported tag tapped", e)
+            return
+        }
+        tagId = tag?.tagId
+        showToast("Tag tapped: $tagId")
+
+//        if (NfcAdapter.ACTION_NDEF_DISCOVERED == intent.action) {
+//            val rawMsgs = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)
+//            if (rawMsgs != null) {
+//                onTagTapped(NfcUtils.getUID(intent), NfcUtils.getData(rawMsgs))
+//            }
+//        }
+    }
+
+    private fun onTagTapped(superTagId: String, superTagData: String) {
+        tag_uid.text = superTagId
+        tag_data.text = superTagData
+    }
 }
