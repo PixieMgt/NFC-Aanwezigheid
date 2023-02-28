d82fa0f9ff571dd1cb53dc7b3e2b9d4c9359830f
PixieMgt <pixiemoortgat@outlook.com>
2023-02-28 13:00:12 +0100

    android project works

---
+++ NFC-Aanwezigheid-Android/app/src/main/java/be/vives/ti/nfc_aanwezigheid/MainActivity.kt
@@ -0,0 +1,11 @@
+package be.vives.ti.nfc_aanwezigheid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
